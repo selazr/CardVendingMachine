3d9d7cd0ff5bade7e80fd55b6f0da60a596bb6c4
selazr <selazr@protonmail.com>
2025-06-14 18:48:34 +0000

    changes

--- backend/config/config.js
+++ backend/config/config.js
@@ -7,7 +7,7 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'postgres'
+    dialect: 'mysql'
   },
   production: {
     username: process.env.DB_USER,
@@ -15,6 +15,6 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'postgres'
+    dialect: 'mysql'
   }
 };
